7278f916f9fb1e91ecc32c4d2e7c8dd0a1aa6b52
Brian Rodriguez <thatbrod@gmail.com>
2016-02-08 23:08:11 -0500

    Refactoring in tests for readability

--- test/Entity.cc
+++ test/Entity.cc
@@ -6,15 +6,13 @@
 #include "Entity.h"
 #include "SimplePODComponents.h"
 
-namespace ae = aetee;
-
 
 TEST(Entity, ReactsToUpdates)
 {
-    int expected{3}, actual{0};
-    auto factory = [&](auto&_) {
-        return std::make_tuple([&](){ actual += 1; });
-    };
+    int expected{3};
+    int actual{0};
+    auto incrementer = [&](){ actual += 1; };
+    auto factory = [&](auto&_) { return std::make_tuple(incrementer); };
     Entity<EchoComponent> dummy{factory};
     dummy.update(1), dummy.update(1), dummy.update(1);
     EXPECT_EQ(expected, actual);
--- test/FactoryTuple.cc
+++ test/FactoryTuple.cc
@@ -10,14 +10,11 @@
 using aetee::operator""_c;
 
 template <typename... T, typename R>
-constexpr bool operator==(const FactoryTuple<T...>& lhs, const R& rhs) {
-    return lhs.as_tuple() == rhs;
-}
-
+constexpr bool operator==(const FactoryTuple<T...>& lhs, const R& rhs)
+    { return lhs.as_tuple() == rhs; }
 template <typename L, typename... T>
-constexpr bool operator==(const L& lhs, const FactoryTuple<T...>& rhs) {
-    return lhs == rhs.as_tuple();
-}
+constexpr bool operator==(const L& lhs, const FactoryTuple<T...>& rhs)
+    { return lhs == rhs.as_tuple(); }
 
 
 TEST(FactoryTuple, EmptyTupleExists)
@@ -28,7 +25,7 @@ TEST(FactoryTuple, EmptyTupleExists)
 
 TEST(FactoryTuple, DefaultProduction)
 {
-    std::tuple<int, int> expected{0, 0};
+    std::tuple<int, int> expected{};
     FactoryTuple<int, int> actual{};
     EXPECT_EQ(2*sizeof(int), sizeof(FactoryTuple<int, int>));
     EXPECT_EQ(expected, actual.as_tuple());
@@ -36,9 +33,10 @@ TEST(FactoryTuple, DefaultProduction)
 
 TEST(FactoryTuple, HomoProduction)
 {
-    std::tuple<int, int, int> expected{2, 1, 3};
-    FactoryTuple<int, int, int> actual{
-        [&](auto& e) { return std::make_tuple(2); }
+    std::tuple<int, int, int> expected
+      { 2, 1, 3 };
+    FactoryTuple<int, int, int> actual
+      { [&](auto& e) { return std::make_tuple(2); }
       , [&](auto& e) { return std::make_tuple(1); }
       , [&](auto& e) { return std::make_tuple(3); }
         };
@@ -47,9 +45,10 @@ TEST(FactoryTuple, HomoProduction)
 
 TEST(FactoryTuple, HeteroProduction)
 {
-    std::tuple<int, float, double> expected{1, 3, 2};
-    FactoryTuple<int, float, double> actual{
-        [&](auto& e) { return std::make_tuple(1); }
+    std::tuple<int, float, double> expected
+      { 1, 3, 2 };
+    FactoryTuple<int, float, double> actual
+      { [&](auto& e) { return std::make_tuple(1); }
       , [&](auto& e) { return std::make_tuple(3); }
       , [&](auto& e) { return std::make_tuple(2); }
         };
@@ -58,9 +57,10 @@ TEST(FactoryTuple, HeteroProduction)
 
 TEST(FactoryTuple, HomoAssignment)
 {
-    std::tuple<int, int, int> expected{2, 1, 3};
-    FactoryTuple<int, int, int> actual{
-        [&](auto& e) { return std::tie(std::get<0>(expected)); }
+    std::tuple<int, int, int> expected
+      { 2, 1, 3 };
+    FactoryTuple<int, int, int> actual
+      { [&](auto& e) { return std::tie(std::get<0>(expected)); }
       , [&](auto& e) { return std::tie(std::get<1>(expected)); }
       , [&](auto& e) { return std::tie(std::get<2>(expected)); }
         };
@@ -69,9 +69,10 @@ TEST(FactoryTuple, HomoAssignment)
 
 TEST(FactoryTuple, HeteroAssignment)
 {
-    std::tuple<int, float, double> expected{1, 3, 2};
-    FactoryTuple<int, float, double> actual{
-        [&](auto& e) { return std::tie(std::get<0>(expected)); }
+    std::tuple<int, float, double> expected
+      { 1, 3, 2 };
+    FactoryTuple<int, float, double> actual
+      { [&](auto& e) { return std::tie(std::get<0>(expected)); }
       , [&](auto& e) { return std::tie(std::get<1>(expected)); }
       , [&](auto& e) { return std::tie(std::get<2>(expected)); }
         };
@@ -80,9 +81,10 @@ TEST(FactoryTuple, HeteroAssignment)
 
 TEST(FactoryTuple, PaddedAssignment)
 {
-    std::tuple<int, char, float, char, char, double, int> expected{1, 'a', 3, 'b', 'c', 2, 0};
-    FactoryTuple<int, char, float, char, char, double, int> actual{
-        [&](auto& e) { return std::tie(std::get<0>(expected)); }
+    std::tuple<int, char, float, char, char, double, int> expected
+      { 1, 'a', 3, 'b', 'c', 2, 0 };
+    FactoryTuple<int, char, float, char, char, double, int> actual
+      { [&](auto& e) { return std::tie(std::get<0>(expected)); }
       , [&](auto& e) { return std::tie(std::get<1>(expected)); }
       , [&](auto& e) { return std::tie(std::get<2>(expected)); }
       , [&](auto& e) { return std::tie(std::get<3>(expected)); }
@@ -95,8 +97,11 @@ TEST(FactoryTuple, PaddedAssignment)
 
 TEST(FactoryTuple, DependantProduction)
 {
-    std::tuple<std::string, std::string> expected{"down the pipe", "out the pipe"};
-    auto simple = [](auto& _) { return std::make_tuple("down the pipe"); };
+    std::tuple<std::string, std::string> expected
+      { "down the pipe", "out the pipe" };
+    auto simple = [](auto& _) {
+        return std::make_tuple("down the pipe");
+    };
     auto dependant = [](auto& e) {
         std::string bottle{e[0_c]};
         bottle.replace(0, 4, "out");
