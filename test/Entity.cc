a78edbe76b2c06f1e2636d93c6dad9d7dfdfe3c5
Brian Rodriguez <thatbrod@gmail.com>
2016-02-10 10:42:44 -0500

    Created a SimpleMotion component, used in testing

--- test/Entity.cc
+++ test/Entity.cc
@@ -7,6 +7,7 @@
 #include "Entity.h"
 #include "PODComponents.h"
 #include "Mimic.h"
+#include "SimpleMotion.h"
 
 using namespace aetee;
 
@@ -14,50 +15,40 @@ using namespace aetee;
 TEST(Entity, ReactsToUpdates)
 {
     int expected{3}, actual{0};
-
+    // Test...
     Entity<MimicC> sample{
-        [&](auto&_) { return std::make_tuple([&](...){ actual += 1; }); }
+        [&](auto&_) { return tupify([&](...){ actual += 1; }); }
         };
     sample.update(1), sample.update(1), sample.update(1);
-
+    // Expectation...
     EXPECT_EQ(expected, actual);
 }
 
 TEST(Entity, UpdateInOrder)
 {
-    int expected{16};
-    int actual{0};
-    auto addOp = [&](...){ actual += 2; };
-    auto dblOp = [&](...){ actual += actual; };
-    auto expOp = [&](...){ actual *= actual; };
-
+    int expected{16}, actual{0};
+    // Test...
     Entity<MimicC, MimicC, MimicC> sample
-      { [&](auto&_) { return std::make_tuple(addOp); }
-      , [&](auto&_) { return std::make_tuple(dblOp); }
-      , [&](auto&_) { return std::make_tuple(expOp); }
+      { [&](auto&_) { return tupify([&](...){ actual += 2; }); }
+      , [&](auto&_) { return tupify([&](...){ actual += actual; }); }
+      , [&](auto&_) { return tupify([&](...){ actual *= actual; }); }
         };
     sample.update(1);
-
+    // Expectation...
     EXPECT_EQ(expected, actual);
 }
 
-//! N.B.:  MAIN MOTIVATION BEHIND USAGE OF FACTORYTUPLE
+// N.B.  THIS IS THE MAIN MOTIVATION BEHIND THE USAGE OF FACTORYTUPLE
 TEST(Entity, ReferenceEarlierMembers)
 {
-    sf::Vector2f expected{0, 0};
-    sf::Vector2f initial{3, 4};
-
-    Entity<PositionC, VelocityC, MimicC> sample
-      { [&](auto& e) { return std::make_tuple(initial); }
-      , [&](auto& e) { return std::make_tuple(-e[0_c].value);  }
-      , [&](auto& e) {
-            auto& p = std::get<PositionC>(e);
-            auto& v = std::get<VelocityC>(e);
-            return std::make_tuple([&](float i) { p.value += (v.value * i); });
-        }};
-    sf::Vector2f& actual = sample[0_c].value;
-
-    EXPECT_EQ(initial, actual);
+    sf::Vector2f expected{0, 0}, initial{3, 4};
+    // Test...
+    Entity<PositionC, VelocityC, SimpleMotionC> sample
+      { [&](auto& e) { return tupify(initial); }
+      , [&](auto& e) { return tupify(-initial);  }
+      , [&](auto& e) { return std::tie(e[0_c], e[1_c]); }
+        };
     sample.update(3), sample.update(-4), sample.update(2);
-    EXPECT_EQ(expected, actual);
+    // Expectation...
+    EXPECT_EQ(expected, sample[0_c].value);
 }
